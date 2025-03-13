76304ea035f5280e198ecb56470bf0b681c3823e
Ryan Yusuf <ryusuf11@gmail.com>
2025-03-13 10:01:00 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
+		domains: ["image.tmdb.org"],
 		minimumCacheTTL: 2678400,
 		remotePatterns: [
 			{
